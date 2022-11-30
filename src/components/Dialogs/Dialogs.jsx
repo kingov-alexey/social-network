6f07c635819db28f7db5be0fec78ecf9ec0faea2
kingov.alexey <kingov_alexey@mail.ru>
2022-11-30 22:02:54 +0600

    lesson 21  done

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,18 @@ import styles from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={styles.dialogs}>
+      <div className={styles.dialogsItems}>
+        <div className={styles.dialog + ' ' + styles.active}>Имя1</div>
+        <div className={styles.dialog}>Имя2</div>
+        <div className={styles.dialog}>Имя3</div>
+        <div className={styles.dialog}>Имя4</div>
+      </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>Hi</div>
+        <div className={styles.message}>How is your learn?</div>
+        <div className={styles.message}>Yo</div>
+      </div>
     </div>
   );
 };
